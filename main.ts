6c719e865ad9a536e562e2ae1eee252a47c44d93
nikolabaric2014 <76596187+nikolabaric2014@users.noreply.github.com>
2020-12-28 10:23:06 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,31 @@
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
-        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
     } else {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
             maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
-            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
                 maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
-                maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+                maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 150)
             }
         } else {
             if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+                maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
                 maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
                 if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
                     maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
                 }
                 if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+                    maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 150)
                 } else {
                     maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
                 }
